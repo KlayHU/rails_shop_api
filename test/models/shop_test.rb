5375442e4dec4fd8a8ac0ec11b90124fd9e7d1d8
hudongrui <hudongrui_klay@163.com>
2023-02-27 09:48:29 +0800

    feat: 补充minitest

--- test/models/shop_test.rb
+++ test/models/shop_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 class ShopTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'valid: shop with all valid things' do
+    shop = Shop.new(name: 'shopTest01', products_count: 0, order_count:1)
+    shop.user = users(:four)
+    assert shop.valid?
+  end
+
+  test 'invalid: shop with taken name' do
+    shop =Shop.new(name: shops(:one).name, products_count: 0, order_count:2)
+    shop.user = users(:five)
+    assert_not shop.valid?
+  end
+
+  test 'invalid: shop with invalid user_id' do
+    shop = Shop.new(name: 'shopTest02', products_count:0, order_count:1)
+    shop.user = users(:two)
+    assert_not shop.valid?
+  end
+
+  test 'invalid: shop with taken user_id' do
+    shop = Shop.new(name: 'shopTest03', products_count:0, order_count:1)
+    shop.user = shops(:one).user
+    assert_not shop.valid?
+  end
+
+  test 'invalid: shop with invalid products_count' do
+    shop = Shop.new(name:'shopTest04', products_count:0, order_count:1.50)
+    shop.user = users(:six)
+    assert_not shop.valid?
+  end
+
 end
